d95b70409509caef9ace80d9daf2a151b123e3bd
Eric Masiello <eric.j.masiello@gmail.com>
2017-09-11 17:18:42 -0400

    Artificially delay redirecting to the landing page after deleting a record. Show "Deleting message"

--- src/components/RestaurantDetail.js
+++ src/components/RestaurantDetail.js
@@ -5,13 +5,16 @@ import ReviewListContainer from '../containers/ReviewListContainer';
 export default class RestaurantDetail extends Component {
   constructor(props) {
     super(props);
+    this.state = { isDeleting: false };
     this.deleteRestaurant = this.deleteRestaurant.bind(this);
   }
   componentDidMount() {
     this.props.load(this.props.match.params.id);
   }
   deleteRestaurant(event) {
+    this.setState({ isDeleting: true });
     this.props.delete(this.props.match.params.id);
+    setTimeout(() => this.props.history.replace('/'), 500);
   }
   render() {
     const { restaurant: {
@@ -24,30 +27,34 @@ export default class RestaurantDetail extends Component {
     } = this.props;
     return (
       <div>
-        <section>
-          <h1>{name} 
-            <small>
-              <Link to={`/restaurant/edit/${id}`}>Edit</Link>
-            </small>
-            <small>
-              <Link
-                onClick={this.deleteRestaurant}
-                to="/"
-              >
-                Delete
-              </Link>
-            </small>
-          </h1>
-          <div>Price: {price}</div>
-          <div>Stars {stars}</div>
-        </section>
-        <section>
-          <h2>Reviews</h2>
-          <ReviewListContainer
-            reviews={reviews}
-            restaurantId={id}
-          />
-        </section>
+        {this.state.isDeleting ? 
+          <div>Deleting...</div> :
+          <div>
+            <section>
+              <h1>{name} 
+                <small>
+                  <Link to={`/restaurant/edit/${id}`}>Edit</Link>
+                </small>
+                <small>
+                  <button
+                    onClick={this.deleteRestaurant}
+                  >
+                    Delete
+                  </button>
+                </small>
+              </h1>
+              <div>Price: {price}</div>
+              <div>Stars {stars}</div>
+            </section>
+            <section>
+              <h2>Reviews</h2>
+              <ReviewListContainer
+                reviews={reviews}
+                restaurantId={id}
+              />
+            </section>
+          </div>
+        }
       </div>
     );
   }
